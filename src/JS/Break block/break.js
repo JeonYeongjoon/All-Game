2759f31ae6690df12d6571c1b8d0de1e1832f515
hohohogugugu <bluehome8626@naver.com>
2021-08-23 08:59:18 +0900

    GameWorld JS

--- src/JS/Break block/break.js
+++ src/JS/Break block/break.js
@@ -1,6 +1,28 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 300;
-canvas.height = 500;
+// 캔버스 크기
+canvas.width = 500;
+canvas.height = 800;
 
+// 기본설정
+let circleX = getRendom(100, 700);
+let circleY = getRendom(250, 500);
+let radius = 20;
+
+// 공그리기
+function circle() {
+    ctx.beginPath();
+    ctx.arc(circleX, circleY, radius, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function getRendom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+circle();
