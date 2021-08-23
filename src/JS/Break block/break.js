847dc039f10f35bf79383e7dc82a454556b274c3
hohohogugugu <bluehome8626@naver.com>
2021-08-23 09:15:12 +0900

    GameWorld JS

--- src/JS/Break block/break.js
+++ src/JS/Break block/break.js
@@ -2,19 +2,31 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 // 캔버스 크기
-canvas.width = 500;
-canvas.height = 800;
+canvas.width = 350;
+canvas.height = 550;
 
 // 기본설정
 let circleX = getRendom(100, 700);
 let circleY = getRendom(250, 500);
-let radius = 20;
+let radius = 10;
+const paddleHeight = 10;
+const paddleWidth = 130;
+const paddleX = (canvas.width-paddleWidth)/2;
 
-// 공그리기
+// 공
 function circle() {
     ctx.beginPath();
-    ctx.arc(circleX, circleY, radius, 0, Math.PI*2);
+    ctx.arc(circleX, circleY, radius, 0, Math.PI*2); // x좌표, y좌표, 원 반지름, 그리는 방향
     ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath(); // 공 그리기
+}
+
+// 패들
+function paddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleWidth); // from x, from y, to x, to y 좌표
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
@@ -26,3 +38,4 @@ function getRendom(min, max) {
 }
 
 circle();
+paddle();
