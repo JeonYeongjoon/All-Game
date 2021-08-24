2c7e542ff14368a6398965b152642adcbb5c9fe1
hohohogugugu <bluehome8626@naver.com>
2021-08-24 10:00:06 +0900

    Break Block Game JS

--- src/JS/Break block/break.js
+++ src/JS/Break block/break.js
@@ -12,8 +12,13 @@ let radius = 10;
 let brickColumnCount;
 let totalBricks;
 let lives;
+
+let game = false;
+let death = false;
+
 const paddleHeight = 10;
 const paddleWidth = 70;
+
 let paddleX = (canvas.width-paddleWidth)/2;
 
 // 벽돌 선언
@@ -27,6 +32,13 @@ for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 4; j++) {
         bricks[i][j] = true;
     }
+}
+
+// 랜덤 함수
+function getRendom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // 공
@@ -53,6 +65,43 @@ function paddle() {
     ctx.closePath();
 }
 
+// 벽돌
+function brick() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 4; j++) {
+            brickX[i] = i * 185 + 10;
+            brickY[j] = j * 50 + 10;
+            if (!bricks[i][j]) { // 점수
+                totalBricks++;
+                if (num == 32) { // 블럭을 다 맞췄을때
+                    let input = confirm("축하드립니다!!!\n다시 시작하겠습니까?");
+                    if (input) {
+                        location.reload();
+                    }
+                    death = true;
+                    game = false;
+                }
+            } else {
+                num = 0;
+            }
+            if (bricks[i][j]) { // 줄에 따른 벽돌 색
+                switch (j) { 
+                    case 0:
+                        ctx.fillStyle = "gray";
+                        break;
+                    case 1:
+                        ctx.fillStyle = "black";
+                        break;
+                    case 2:
+                        ctx.fillStyle = "rgb(0,0,128)";
+                        break;
+                }
+                ctx.fillRect(brickX[i], brickY[j], 175, 40);
+            }
+        }
+    }
+}
+
 // 남은 벽돌 갯수
 function drawOverBlock() {
     ctx.font = "16px Arial";
@@ -67,25 +116,17 @@ function drawLive() {
     ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 
-// 랜덤 함수
-function getRendom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 setInterval(function() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     circle();
     paddle();
-    drawBricks();
     drawOverBlock();
     drawLive();
+    brick();
 }, 20)
     
 function move(event) {
     paddleX = event.offsetX;
-    console.log(paddleX);
 }
 
 canvas.addEventListener('mousemove', move);
