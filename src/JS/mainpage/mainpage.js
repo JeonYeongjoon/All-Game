ba3503cca7f25c5291789ee42de9a57065205895
hohohogugugu <bluehome8626@naver.com>
2021-08-25 22:50:28 +0900

    JavaScript Game

--- src/JS/mainpage/mainpage.js
+++ src/JS/mainpage/mainpage.js
@@ -1,8 +1,6 @@
 const Game1 = document.getElementsByClassName("Game1")[0];
 const Game2 = document.getElementsByClassName("Game2")[0];
 const Game3 = document.getElementsByClassName("Game3")[0];
-const Game4 = document.getElementsByClassName("Game4")[0];
-const Game5 = document.getElementsByClassName("Game5")[0];
 
 function clickGame1() {
     location.href = "./BreakBlock.html";
@@ -12,21 +10,8 @@ function clickGame2() {
     location.href = "./DinoJump.html";
 }
 
-function clickGame3() {
-    location.href = "";
-}
-
-function clickGame4() {
-    location.href = "";
-}
-
-function clickGame5() {
-    location.href = "";
-}
 
 
 Game1.addEventListener("click", clickGame1);
 Game2.addEventListener("click", clickGame2);
 Game3.addEventListener("click", clickGame3);
-Game4.addEventListener("click", clickGame4);
-Game5.addEventListener("click", clickGame5);
