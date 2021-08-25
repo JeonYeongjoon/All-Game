3e177a2eccc76faff4ac950f12599597717bbadc
hohohogugugu <bluehome8626@naver.com>
2021-08-26 00:34:17 +0900

    가위바위보 게임

---
+++ src/JS/rspGame/rspGame.js
@@ -0,0 +1,6 @@
+const images = ["0.jpg","1.jpg","2.jpg"]
+const choseImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.getElementsByClassName("AI")[0];
+
+bgImage.style.backgroundImage=`url('./img/${choseImage}')`;
+
