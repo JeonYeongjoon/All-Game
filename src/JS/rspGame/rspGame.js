93e747dfa5a5d7c7c5f8079f53825b88491a8336
hohohogugugu <bluehome8626@naver.com>
2021-08-26 08:22:51 +0900

    가위바위보 게임

--- src/JS/rspGame/rspGame.js
+++ src/JS/rspGame/rspGame.js
@@ -1,6 +1,12 @@
 const images = ["0.jpg","1.jpg","2.jpg"]
 const choseImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.getElementsByClassName("AI")[0];
+const exit = document.getElementsByClassName("exit")[0];
 
 bgImage.style.backgroundImage=`url('./img/${choseImage}')`;
 
+function onClickexit() {
+    location.href = "file:///C:/code/js/All-Game/src/html/index.html";
+}
+
+exit.addEventListener("click", onClickexit);
