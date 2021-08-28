859add77ebd9c89f8c505378e20fd05470a68bdd
hohohogugugu <bluehome8626@naver.com>
2021-08-28 21:44:12 +0900

    UPDOWN GAME

---
+++ src/JS/upDown/updown.js
@@ -0,0 +1,7 @@
+const exit = document.getElementsByClassName("exit")[0];
+
+function onClickexit() {
+    location.href = "file:///C:/code/js/All-Game/src/html/index.html";
+}
+
+exit.addEventListener('click', onClickexit);
