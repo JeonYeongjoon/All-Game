cf1650742350cbc1bd8ccf38da46fc8d5c6a131e
hohohogugugu <bluehome8626@naver.com>
2021-08-29 21:45:33 +0900

    UPDOWN GAME

--- src/JS/upDown/updown.js
+++ src/JS/upDown/updown.js
@@ -1,7 +1,26 @@
 const exit = document.getElementsByClassName("exit")[0];
+const bord = document.getElementsByClassName("bord")[0];
+const clickrandom = document.getElementsByClassName("printrandom")[0];
+
+// random함수 최소,최대 값 정해주기
+const min = 0;
+const max = 100;
+let num = getRandom();
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function onClickexit() {
     location.href = "file:///C:/code/js/All-Game/src/html/index.html";
 }
 
+function print() {
+    console.log(num);
+    bord.innerText = "숫자가 출력되었습니다.";
+}
+
+clickrandom.addEventListener('click', print);
 exit.addEventListener('click', onClickexit);
