e3810de8baf784195acecd8492cafa80f302d085
hohohogugugu <bluehome8626@naver.com>
2021-08-30 09:05:33 +0900

    UPDOWN GMAE

--- src/JS/upDown/updown.js
+++ src/JS/upDown/updown.js
@@ -5,16 +5,31 @@ const clickrandom = document.getElementsByClassName("printrandom")[0];
 // random함수 최소,최대 값 정해주기
 const min = 0;
 const max = 100;
+
+// 랜덤 함수
 const randomNum = Math.random() * 100;
 const randomNumFloor = Math.floor(randomNum);
 
+// 나가기 버튼
 function onClickexit() {
     location.href = "file:///C:/code/js/All-Game/src/html/index.html";
 }
 
+
+// 숫자 프린트
 function print() {
     console.log(randomNumFloor);
-    board.innerText = randomNumFloor;
+    board.innerText = "1~100\n사이의 수";
+}
+
+// 숫자 입력
+function input() {
+
+}
+
+// 숫자 비교
+function comparison() {
+ 
 }
 
 clickrandom.addEventListener('click', print);
