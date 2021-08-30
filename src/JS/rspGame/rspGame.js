85a0177e7fef23b8a006b09d7efb32ba76e6f5de
hohohogugugu <bluehome8626@naver.com>
2021-08-30 23:19:47 +0900

    GAME

--- src/JS/rspGame/rspGame.js
+++ src/JS/rspGame/rspGame.js
@@ -18,20 +18,41 @@ function randomValue(array) {
 }
 
 function rock() {
+    console.log(printrsp);
     boardUser.innerText = "바위";
+
+    if (printrsp == "가위"){
+        boardAi.innerText = "승리";
+    } else if (printrsp == "바위") {
+        boardAi.innerText = "비김";
+    } else boardAi.innerText = "패배";
 }
 
 function scissor() {
+    console.log(printrsp);
     boardUser.innerText = "가위";
+
+    if (printrsp == "가위"){
+        boardAi.innerText = "비김";
+    } else if (printrsp == "바위") {
+        boardAi.innerText = "패배";
+    } else boardAi.innerText = "승리";
 }
 
 function paper() {
+    console.log(printrsp);
     boardUser.innerText = "보";
+
+    if (printrsp == "가위"){
+        boardAi.innerText = "패배";
+    } else if (printrsp == "바위") {
+        boardAi.innerText = "승리";
+    } else boardAi.innerText = "비김";
 }
 
 function rspRandom() {
-    console.log("1");
     boardAi.innerText = "가위 바위 보!";
+    console.log(printrsp);
 }
 
 function reset() {
