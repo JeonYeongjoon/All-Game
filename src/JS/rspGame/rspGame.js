681f3186ea7367f14281be8b8aad70ca0a886e72
hohohogugugu <bluehome8626@naver.com>
2021-08-30 19:39:43 +0900

    GAME

--- src/JS/rspGame/rspGame.js
+++ src/JS/rspGame/rspGame.js
@@ -1,13 +1,17 @@
 const images = ["0.jpg","1.jpg","2.jpg"]
 const choseImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.getElementsByClassName("AI")[0];
+const board = document.getElementsByClassName("boardAi")[0];
+const randombtn = document.getElementsByClassName("randombtn")[0];
 const exit = document.getElementsByClassName("exit")[0];
 
-bgImage.style.backgroundImage=`url('./img/${choseImage}')`;
+function rspRandom() {
+    board.innerText = "가위 바위 보!";
+}
 
 // 나가기 버튼
 function onClickexit() {
     location.href = "file:///C:/code/js/All-Game/src/html/index.html";
 }
 
+randombtn.addEventListener("click", rspRandom);
 exit.addEventListener("click", onClickexit);
